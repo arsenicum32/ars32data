ea7bb2ac7c1def4cb0909d168a13df583cd0b15e
arsenicum32 <zombiehot@yandex.ru>
2016-05-05 21:13:16 +0300

    xhange

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
+if(typeof $ == 'undefined'){
+  console.log("include jquery");
+  document.write('<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>');
+}
 var atext = {
-  loc: 'en'
+  navobj: navigator.language.slice(0,2) || 'en',
+  dthelp: {
+    gdata: function(file, callback, err){
+      $.ajax({
+         url:"https://raw.githubusercontent.com/arsenicum32/ars32data/gh-pages/"+atext.navobj+"/" + file + '.json',
+         dataType: 'json', // Notice! JSONP <-- P (lowercase)
+         success: callback,
+         error: err
+      });
+    }
+  },
+  data: {},
+  fire: function(callback){
+    for(var n in atext.dtjson){
+      atext.dthelp.gdata( atext.dtjson[n] , function(data){ atext.data[n] = data }, function(err){ console.log(err); });
+    }
+    if (callback) callback(atext.data);
+  },
+  dtjson: ['about']
 }
