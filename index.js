06bda2fbe8c4c084f4190215353f0ae7fb037f15
arsenicum32 <zombiehot@yandex.ru>
2016-05-05 23:52:12 +0300

    ref

--- index.js
+++ index.js
@@ -4,22 +4,21 @@ if(typeof $ == 'undefined'){
 }
 var atext = {
   navobj: navigator.language.slice(0,2) || 'en',
-  dthelp: {
-    gdata: function(file, callback, err){
+  g: function(file, callback, err){
+      !callback?callback=function(){}:void(0);!err?err=function(){}:void(0);!file?return;void(0);
       $.ajax({
          url:"https://raw.githubusercontent.com/arsenicum32/ars32data/gh-pages/"+atext.navobj+"/" + file + '.json',
          dataType: 'json', // Notice! JSONP <-- P (lowercase)
          success: callback,
          error: err
       });
-    }
   },
   data: {},
   getany: function(arr){
     var jsonprofile  =  {};
     for(var i = 0; i < arr.length; i++){
       if(typeof arr[i] === typeof ''){
-        atext.dthelp.gdata( arr[i] , function(data){
+        atext.g( arr[i] , function(data){
           jsonprofile[ arr[i] ] = data;
           if(i == arr.length - 1) return jsonprofile;
         }, function(err){ console.log(err); });
@@ -29,12 +28,12 @@ var atext = {
   fire: function(callback){
     var fin = {};
     for(var n = 0; n< atext.dtjson.length; n++ ){
-      atext.dthelp.gdata( atext.dtjson[n] , function(data){
+      atext.g( atext.dtjson[n] , function(data){
         atext.data[ atext.dtjson[n] ] = data;
         fin[ atext.dtjson[n] ] = data;
-        if( n === atext.dtjson.length -1 && callback) callback(fin);
       }, function(err){ console.log(err); });
     }
+    if (callback) callback(fin);
   },
-  dtjson: ['about','test']
+  dtjson: ['about']
 }
