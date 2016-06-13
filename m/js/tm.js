d7ea31864c8840e68e9000b87bf207cd327da04e
arsenicum32 <zombiehot@yandex.ru>
2016-06-14 02:12:20 +0300

    go

--- m/js/tm.js
+++ m/js/tm.js
@@ -1,2 +1,10 @@
 var t = window.location.href.split('/')[window.location.href.split('/').length - 2];
-alert(t);
+t = parseInt(t);
+
+window.onload = function(){
+  setTimeout(function(){
+    if(t>1){
+      window.location.href = 'http://ars32.gq/m/' + (t -1);
+    }
+  }, 1000);
+}
