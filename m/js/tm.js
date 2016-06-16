7f75cee8296063e2f4eafefde7b80a08387ae85d
arsenicum32 <zombiehot@yandex.ru>
2016-06-16 04:39:33 +0300

    afefa

--- m/js/tm.js
+++ m/js/tm.js
@@ -4,6 +4,8 @@ t = parseInt(t);
 window.onload = function(){
   setTimeout(function(){
     if(t>1){
+      var audio = new Audio('../audio/Hozier.mp3');
+      audio.play();
       window.location.href = 'http://ars32.gq/m/' + (t -1);
     }else if (t==1){
       window.location.href = 'http://ars32.gq/m/love';
